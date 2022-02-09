d9a66220087f0e76ef584ebd0431e9170852df32
satyam mishra <satyam.mishra975@gmail.com>
2022-02-09 16:44:49 +0530

    add mongo connection in seperate fn

--- src/hooks.js
+++ src/hooks.js
@@ -5,13 +5,14 @@ import {dev} from '$app/env'
 export async function handle({ event, resolve }) {
 	const {locals} = event 
 	const mongo_connect = await mongo_db_connection()
+	const db_name = 'url_shortener'
 	locals.db = await mongo_connect.db(db_name)
 	const response = await resolve(event)
 	return response;
 }
 
 async function mongo_db_connection(){
-	const db_name = 'url_shortener'
+	
 	const mongo_url = dev?"mongodb://localhost:27017/":import.meta.env.VITE_MONGO_URL;
 	const mongo_connect_options = dev?{useUnifiedTopology: true }:{useNewUrlParser: true, useUnifiedTopology: true, serverApi : ServerApiVersion.v1 }
 	const mongo_connect = await MongoClient.connect(mongo_url,mongo_connect_options).catch((err)=>console.log('Error connecting mongodb',err));
