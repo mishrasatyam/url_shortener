2e31fe9d189eda3d4c72a75b28a4b11d3b373535
satyam mishra <satyam.mishra975@gmail.com>
2022-02-09 16:43:34 +0530

    add mongo connection in seperate fn

--- src/hooks.js
+++ src/hooks.js
@@ -1,15 +1,19 @@
 import {MongoClient,ServerApiVersion} from 'mongodb'
 import {dev} from '$app/env'
 
-const db_name = 'url_shortener'
-const mongo_url = dev?"mongodb://localhost:27017/":import.meta.env.VITE_MONGO_URL;
-const mongo_connect_options = dev?{useUnifiedTopology: true }:{useNewUrlParser: true, useUnifiedTopology: true, serverApi : ServerApiVersion.v1 }
-const mongo_connect = await MongoClient.connect(mongo_url,mongo_connect_options).catch((err)=>console.log('Error connecting mongodb',err));
-
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
 	const {locals} = event 
+	const mongo_connect = await mongo_db_connection()
 	locals.db = await mongo_connect.db(db_name)
 	const response = await resolve(event)
 	return response;
+}
+
+async function mongo_db_connection(){
+	const db_name = 'url_shortener'
+	const mongo_url = dev?"mongodb://localhost:27017/":import.meta.env.VITE_MONGO_URL;
+	const mongo_connect_options = dev?{useUnifiedTopology: true }:{useNewUrlParser: true, useUnifiedTopology: true, serverApi : ServerApiVersion.v1 }
+	const mongo_connect = await MongoClient.connect(mongo_url,mongo_connect_options).catch((err)=>console.log('Error connecting mongodb',err));
+	return mongo_connect
 }
